55e000356002603c13e8baa636603eca1c80bc95
Akke Luukkonen <akke@luukkonen.dev>
2019-12-01 14:10:54 +0200

    Add error handling to sticker set creation

--- rubus/stickers.py
+++ rubus/stickers.py
@@ -31,13 +31,17 @@ def _sticker_set_create_new(query):
     bot = query.bot.bot
     sticker_set_name = f"{user['username']}_by_{bot['username']}"
 
-    with open(DEFAULT_STICKER_SET_PNG, 'rb') as png_sticker:
-        success = query.bot.create_new_sticker_set(
-            user['id'],
-            sticker_set_name,
-            sticker_set_name,  # Use the name also as the title for now
-            png_sticker,
-            DEFAULT_STICKER_SET_EMOJI)
+    try:
+        with open(DEFAULT_STICKER_SET_PNG, 'rb') as png_sticker:
+            success = query.bot.create_new_sticker_set(
+                user['id'],
+                sticker_set_name,
+                sticker_set_name,  # Use the name also as the title for now
+                png_sticker,
+                DEFAULT_STICKER_SET_EMOJI)
+    except telegram.error.BadRequest:
+        logger.exception("Failed unexpectedly!")
+        success = False
 
     if success:
         query.edit_message_text(text=f"Created a new sticker set named: {sticker_set_name}")
