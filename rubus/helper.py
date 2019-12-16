9c8d0169069f5b2d1b19b4277ae0e54e86b7a604
Akke Luukkonen <akke@luukkonen.dev>
2019-12-16 18:14:59 +0200

    Add JSON based configuration file
    Simultaneously added helper.config_load() to easily fetch the data.

--- rubus/helper.py
+++ rubus/helper.py
@@ -1,8 +1,22 @@
 """
 General helper functions which can be shared between multiple submodules.
 """
+import importlib.resources
+import json
 
 from telegram.ext import ConversationHandler
+
+
+def config_load():
+    """Load stored configuration from disk
+
+    Configuration should be stored as JSON along with the module files.
+
+    :return: Dictionary of the configuration
+    """
+    config_text = importlib.resources.read_text("rubus", "config.json")
+    data = json.loads(config_text)
+    return data
 
 
 def confused(update, context):  # pylint: disable=unused-argument
